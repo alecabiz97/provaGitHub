ae5a46744db5802e316074a2f99d8fce37a8f855
alecabiz97 <alecabiz97@gmail.com>
2020-10-30 17:11:19 +0100

    Create two file. main (empty) and Operazioni.py

---
+++ Operazioni.py
@@ -0,0 +1,18 @@
+
+def sum(a,b):
+    '''
+    Make the sum of two number
+    :param a: int or float
+    :param b: int or float
+    :return: a +b
+    '''
+    pass
+
+def multiplication(a,b):
+    '''
+    Make the multiplication between a and b
+    :param a:
+    :param b:
+    :return:
+    '''
+    pass
